82fd7023852e183b1f715d1a8f9bf74b8403b790
shaksham08 <shakshamsinha@outlook.com>
2020-04-02 16:06:59 +0530

    adding folder

--- projbackend/controllers/auth.js
+++ projbackend/controllers/auth.js
@@ -1,8 +1,30 @@
 const User = require("../models/user.js");
-
+const { check, validationResult } = require('express-validator');
 
 exports.signup = (req, res) => {
-    const user = new User(req.body);
+   
+   const errors = validationResult(req)
+
+   if(!errors.isEmpty()){
+       return res.status(422).json({
+           error : errors.array()[0].msg
+       })
+   }
+   
+    const user = new User(req.body);  //creating the object
+    user.save((err,user)=>{
+        if(err){
+            return res.status(400).json({
+                err : "Not able to save use in DB"
+            })
+        }
+        res.json({
+            name : user.name,
+            email : user.email,
+            id: user._id
+        });
+    });
+
 };
 
 
--- projbackend/routes/auth.js
+++ projbackend/routes/auth.js
@@ -1,8 +1,13 @@
 var express = require('express')
 var router = express.Router()
+const { check, validationResult } = require('express-validator');
 const {signout,signup} = require("../controllers/auth.js")
 
-router.post("/signup", signup);
+router.post("/signup",[
+    check("name","name should be at least 3 character").isLength({min:3}),
+    check("email", "email is required").isEmail(),
+    check("password", "password should be atleast 3 char").isLength({ min: 3 ,max:32})
+], signup);
 router.get("/signout" ,signout );
 
 module.exports = router;
